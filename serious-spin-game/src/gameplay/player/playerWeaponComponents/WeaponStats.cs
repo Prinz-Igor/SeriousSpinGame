2a64436122cd0e0fd1faf57e315487bc717c9134
Ian Chilla <Prinzigor9@googlemail.com>
2026-06-21 12:59:27 +0200

    adds 2 Weapons for Testing

--- serious-spin-game/src/gameplay/player/playerWeaponComponents/WeaponStats.cs
+++ serious-spin-game/src/gameplay/player/playerWeaponComponents/WeaponStats.cs
@@ -4,9 +4,18 @@ using System;
 [GlobalClass]
 public partial class WeaponStats : Resource
 {
+
+    
     [Export] String name = "PlaceholderWeapon";
     [Export] Texture2D weaponSprite;
     [Export] int projectiledamage = 1;
     [Export] int projectileSpeed = 5;
+    [Export] int magazineSize = 5;
+
+    [Export] int weaponCooldown = 2;
+
+    [Export] int salvoSize = 1; 
+
+
     
 }
