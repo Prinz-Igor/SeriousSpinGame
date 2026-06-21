07b752d8b76b8270bf282c43c4e924237f2555b4
Ian Chilla <Prinzigor9@googlemail.com>
2026-06-21 16:02:08 +0200

    added support for projectile object pooling in weaponComponent

--- serious-spin-game/src/gameplay/player/playerWeaponComponent/WeaponComponent.cs
+++ serious-spin-game/src/gameplay/player/playerWeaponComponent/WeaponComponent.cs
@@ -1,5 +1,7 @@
 using Godot;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
 
 public partial class WeaponComponent : Node2D
@@ -7,12 +9,17 @@ public partial class WeaponComponent : Node2D
 	[Export] WeaponStats initialWeapon;
 	[Export] Sprite2D weaponSprite;
 	WeaponStats stats;
+	ArrayList projectilepool; 
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		stats = (WeaponStats)initialWeapon.Duplicate();
 		weaponSprite.Texture = stats.weaponSprite; 
+		for (int i = 0; i < stats.magazineSize; i++)
+		{
+			projectilepool.Add("Bang");
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,6 +30,9 @@ public partial class WeaponComponent : Node2D
 
 	public void OnPLayerFireWeapon()
 	{
-		
+		GD.Print(projectilepool[0]);
 	}
+	
+
+	
 }
